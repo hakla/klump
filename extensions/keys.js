d663dde0dfcae4081046e6a0ee1dc5ea677a1cfc
Klaus Harrer <klaus@hakla.at>
2020-03-10 12:44:30 +0100

    Added mountKey function to extensions/key

--- extensions/keys.js
+++ extensions/keys.js
@@ -1,34 +1,45 @@
 if (window.o != null) {
   (function() {
+    o.extend('waitForKey', function(combo) {
+      return function(next, context) {
+        var el = o.getElement(context, 'body');
+
+        createKeyHandler(el, combo, function() {
+          next(context);
+        });
+      };
+    });
+
     o.extend('registerKey', function(combo, cb) {
       return function(next, context) {
-        var combinations = [];
         var el = o.getElement(context, 'body');
-        var counter = 0;
 
-        var parts = combo.split(',');
-
-        for (var i = 0; i < parts.length; ++i) {
-          combinations.push(mapCombo(parts[i].trim()));
-        }
-
-        el.addEventListener('keydown', function keyDownHandler(event) {
-          if (o.matches(event, combinations[counter]) === true) {
-            if (combinations.length > counter + 1) {
-              ++counter;
-            } else {
-              cb(context);
-
-              counter = 0;
-            }
-          } else {
-            counter = 0;
-          }
+        createKeyHandler(el, combo, function() {
+          cb(context);
         });
 
         next();
       };
     });
+
+    function createKeyHandler(el, combination, cb) {
+      var combinations = parseCombination(combination);
+      var counter = 0;
+
+      el.addEventListener('keydown', function keyDownHandler(event) {
+        if (o.matches(event, combinations[counter]) === true) {
+          if (combinations.length > counter + 1) {
+            ++counter;
+          } else {
+            cb();
+
+            counter = 0;
+          }
+        } else {
+          counter = 0;
+        }
+      });
+    }
 
     function mapCombo(combination) {
       var o = {};
@@ -49,6 +60,18 @@ if (window.o != null) {
       }
 
       return o;
+    }
+
+    function parseCombination(combination) {
+      var combinations = [];
+
+      var parts = combination.split(',');
+
+      for (var i = 0; i < parts.length; ++i) {
+        combinations.push(mapCombo(parts[i].trim()));
+      }
+
+      return combinations;
     }
   })();
 }
