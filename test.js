cf497922b10d648745c88fd1052c85e43889cb3a
Klaus Harrer <klaus@hakla.at>
2020-03-10 12:44:40 +0100

    Updated test script

---
+++ test.js
@@ -0,0 +1,15 @@
+(function() {
+  load(
+    function(o) {
+      o()
+        .waitForKey('shift alt e')
+        .select('#test')
+        .log()
+        .execute(function(context) {
+          context.el.value = Math.round(Math.random() * 10 + 1)
+        })
+        .setStyle('border: none')
+    },
+    ['log', 'control', 'styles', 'keys']
+  );
+})();
