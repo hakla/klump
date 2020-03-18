6c76a853c566eb12e0fbb43e738dd4ebc85669e0
Klaus Harrer <klaus@hakla.at>
2020-03-18 22:14:50 +0100

    feature(prompt) add new prompt extension

---
+++ extensions/prompt.js
@@ -0,0 +1,33 @@
+if (window.o != null) {
+  (function() {
+    o.extend('prompt', prompt);
+
+    o.extend('promptNewValue', function(question, maybeDefaultValue) {
+      return [prompt(question), promptNewValue(maybeDefaultValue)];
+    });
+
+    function prompt(question, maybeDefaultValue) {
+      return function(next, context) {
+        var answer = window.prompt(question);
+
+        next({
+          answer: answer,
+        });
+      };
+    }
+
+    function promptNewValue(maybeDefaultValue) {
+      return function(next, context) {
+        var el = o.getElement(context);
+
+        if (el && el.value !== undefined) {
+          if (context.answer) {
+            el.value = context.answer;
+          } else if (maybeDefaultValue) {
+            el.value = o.evaluate(maybeDefaultValue);
+          }
+        }
+      };
+    }
+  })();
+}
