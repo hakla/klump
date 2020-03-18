6a72dc6c40dbdba2aadd24cce2101e8708214b62
Klaus Harrer <klaus@hakla.at>
2020-03-18 23:16:46 +0100

    feature(test) update test script

--- test.js
+++ test.js
@@ -15,6 +15,16 @@
       o()
         .waitForKey('ctrl+*')
         .log('hit asdf');
+
+      o().registerKey('ctrl+1', o.pipe(pipe1, pipe2));
+
+      function pipe1(o) {
+        o.log('1');
+      }
+
+      function pipe2(o) {
+        o.log('2');
+      }
     },
     ['log', 'control', 'styles', 'keys', 'prompt']
   );
