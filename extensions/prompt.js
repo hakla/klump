ad95c58663672a5d8dc3f3ad9f6b9265207e874e
Klaus Harrer <klaus@hakla.at>
2020-03-18 23:55:54 +0100

    feature(prompt) set the defaultValue directly in the prompt

--- extensions/prompt.js
+++ extensions/prompt.js
@@ -3,12 +3,12 @@ if (window.o != null) {
     o.extend('prompt', prompt);
 
     o.extend('promptNewValue', function(question, maybeDefaultValue) {
-      return [prompt(question), promptNewValue(maybeDefaultValue)];
+      return [prompt(question, maybeDefaultValue), updateValue()];
     });
 
     function prompt(question, maybeDefaultValue) {
       return function(next, context) {
-        var answer = window.prompt(question);
+        var answer = window.prompt(question, o.evaluate(maybeDefaultValue));
 
         next({
           answer: answer,
@@ -16,15 +16,13 @@ if (window.o != null) {
       };
     }
 
-    function promptNewValue(maybeDefaultValue) {
+    function updateValue() {
       return function(next, context) {
         var el = o.getElement(context);
 
         if (el && el.value !== undefined) {
           if (context.answer) {
             el.value = context.answer;
-          } else if (maybeDefaultValue) {
-            el.value = o.evaluate(maybeDefaultValue);
           }
         }
 
