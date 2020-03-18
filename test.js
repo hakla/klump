52bddfab45fc3c99f05026b25b0849d00ad9ae9c
Klaus Harrer <klaus@hakla.at>
2020-03-18 22:56:05 +0100

    feature(test) update test script

--- test.js
+++ test.js
@@ -7,6 +7,14 @@
         .log()
         .promptNewValue('Test?', () => Math.round(Math.random() * 10 + 1))
         .setStyle('border: none');
+
+      o()
+        .waitForKey('ctrl+alt+2')
+        .log('hit ctrl+alt+2');
+
+      o()
+        .waitForKey('ctrl+*')
+        .log('hit asdf');
     },
     ['log', 'control', 'styles', 'keys', 'prompt']
   );
